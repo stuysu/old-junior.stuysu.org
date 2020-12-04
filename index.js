fce8b6dff955ce3dbc7f24479b7ed8f4b8ee63b6
Myles Pasetsky <mp3blue99@gmail.com>
2020-12-04 16:32:27 -0500

    Clean up test code

--- index.js
+++ index.js
@@ -45,18 +45,13 @@ function errorHandler(err, req, res, next) {
 
 // DATABASE
 
+const { sequelize } = require('./models');
+
+// TODO: add function to by default populate the subs model
+
 function setup(db) {
     return db.sync();
 }
-
-const { sequelize } = require('./models');
-
-// DATABASE
-
-//async function setup(db) {
-//    await sequelize.models.Subs.create({ sub: '116704902793588850858' }); 
-//    return db.sync(); 
-//}
 
 // ROUTES
 
