b07d1b0a1066f901417cc4a6dce4f4827b6790a8
Myles Pasetsky <mp3blue99@gmail.com>
2020-12-04 19:01:59 -0500

    Add feedback to admin panel

---
+++ public/javascripts/admin-alerts.js
@@ -0,0 +1,99 @@
+class AlertManager {
+
+    constructor(clearAfter = 10000) {
+        this.root = document.getElementById("alerts");
+        this.types = [
+            "primary",
+            "secondary",
+            "success",
+            "danger",
+            "warning", 
+            "info",
+            "light",
+            "dark"  
+        ];
+
+        this.classes = [
+            "alert",
+            "alert-dismissible",
+            "fade",
+            "show"
+        ];
+
+        this.clearAfterMs = clearAfter;
+
+        this.refreshRate = 100; // 0.1 seconds
+    }
+
+    isLevelValid(level) {
+        return this.types.indexOf(level) !== -1;
+    }
+
+    addAlert(title, message, level='primary') {
+        level = level.toLowerCase();
+        level = this.isLevelValid(level) ? level : this.types[0];
+
+        const alert = document.createElement('div');
+
+        alert.classList.add('alert-' + level);        
+        for (let bsClass of this.classes) {
+            alert.classList.add(bsClass);
+        }
+
+        if (title !== "")
+            title += ':';
+
+        alert.innerHTML = `<strong>${title}</strong> ${message}`;
+
+        const dismissButton = document.createElement('button');
+
+        dismissButton.type = "button";
+        dismissButton.classList.add("close");
+        dismissButton.setAttribute("data-dismiss", "alert");
+        dismissButton.setAttribute("aria-label", "Close");
+        dismissButton.setAttribute("data-created-at", `${new Date().getTime()}`);
+
+        const buttonText = document.createElement('span');
+        buttonText.setAttribute("aria-hidden", "true");
+        buttonText.innerHTML = "&times;";
+
+        dismissButton.appendChild(buttonText);
+
+        alert.appendChild(dismissButton);
+
+        this.root.appendChild(alert);
+
+    }
+
+    deleteOld() {
+        const alerts = this.root.getElementsByClassName(this.classes[0]);
+
+        const timeNow = new Date().getTime();
+
+        for (let alert of alerts) {
+
+            let createdAt = Number(alert.lastChild.getAttribute("data-created-at"));
+
+            if (timeNow - createdAt > this.clearAfterMs) {
+                alert.lastChild.click();
+            }
+
+        }
+    }
+
+    deleteAll() {
+        while (this.root.firstChild) {
+            this.root.removeChild(this.root.firstChild);
+        }
+    }
+
+}
+
+const alertManager = new AlertManager(7_500 /* value in ms */);
+
+setInterval(
+    () => {
+        alertManager.deleteOld();
+    },
+    alertManager.refreshRate
+);
--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -4,61 +4,208 @@ function getInputs(id) {
         alias: document.getElementById(id + '-alias'),
         url: document.getElementById(id + '-url'),
         thread: document.getElementById(id + '-thread'),
-		update: document.getElementById(id + '-update')
+        update: document.getElementById(id + '-update')
     };
 }
 
-function updateLink(id) {
+async function removeLink(id) {
+    const { thread } = getInputs(id);
+    
+    thread.remove();
 
-    const url = 'http://localhost:3001/links';
-    // const data = {
+    let response = await fetch('/links/' + id, {
+        method: 'DELETE', 
+        mode: 'cors', 
+        cache: 'no-cache', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', 
+        referrerPolicy: 'no-referrer',
+    });
+
+    response = await response.json();
+
+    // if response.id is undefined, response failed
+    if (response.id) {
+
+        if (response.deleted === false) {
+
+            alertManager.addAlert('Failure', 'did not find a link to delete', 'secondary');
         
-    // };
+        } else {
 
-    console.log(`${id} =-> ${typeof id}`);
+            alertManager.addAlert('Success', `deleted link`, 'warning');
+
+        }
+
+    } else {
+
+        alertManager.addAlert('Failure', `received an error from the server`, "danger");
+    
+    }
+
+}
+
+async function updateLink(id) {
+
+    const { alias, url } = getInputs(id);
+    let response = await fetch("/links", {
+        method: "PUT",
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({
+            id: id,
+            alias: alias.value,
+            url: url.value
+        })
+    });
+
+    response = await response.json(); 
+
+    if (response.error) {
+
+        alertManager.addAlert('Failure', `received an error from the server`, "danger");
+
+    } else {
+
+        if (response.found && (response.updatedUrl || response.updatedAlias)) {
+            let changedMessage = '';
+            changedMessage += (response.updatedUrl ? (`"${response.old.url}" to "${response.url}"`) : "");
+            changedMessage += (response.updatedAlias ? (`${response.updatedUrl ? ", ":""}"${response.old.alias}" to "${response.alias}"`) : "");
+            
+            alertManager.addAlert('Success', `changed ${changedMessage}`, 'primary');
+        } else {
+            alertManager.addAlert('', `Entry not found or no changes were made`, 'secondary');
+        }
+
+    }
+
+}
+
+async function addLink() {
+
+    const { preview, alias, url } = getInputs('add');
+
+    let response = await fetch("/links", {
+        method: "POST",
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({
+            alias: alias.value,
+            url: url.value
+        })
+    });
+
+    response = await response.json();
+
+    if (response.created) {
+
+        addLinkToPage(
+            response.id,
+            response.alias,
+            response.url
+        );
+
+        alias.value = '';
+        url.value = '';
+        preview.innerHTML = '';
+    
+        alertManager.addAlert('Success', `created "${response.alias}" with redirect to "${response.url}`, "success");
+
+    } else {
+        
+        alertManager.addAlert('Failure', `failed to create link`, "warning");
+
+    }
+
 }
 
 function updatePreview(id) {
     const { preview, alias, url } = getInputs(id);
-    
+
     preview.innerHTML = alias.value;
     preview.href = url.value;
 
-    console.log(`${id} =-> ${typeof id}`);
 }
-async function addLink() {
-	const { preview, alias, url, update, thread } = getInputs('add');
-	const response = await fetch("/links", { 
-		method: "POST",
-		mode: 'cors',
-		cache: 'no-cache',
-		credentials: 'same-origin',
-		headers: {
-			'Content-Type':'application/json'
-		},
-		redirect: 'follow',
-		referrerPolicy: 'no-referrer',
-		body: JSON.stringify({
-			alias: alias.value,
-			url: url.value
-		})
-	});
-	
-}
-async function removeLink(id) {
-	const { preview, alias, url, update, thread } = getInputs(id);
-	thread.remove();
-	return await fetch('/links/'+id,
-		{
-			method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
-			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin', // include, *same-origin, omit
-			headers: {
-				'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-		}); // body data type must match "Content-Type" header
+
+// look away
+function addLinkToPage(id, alias, url) {
+
+    // get table body
+    const mainLinks = document.getElementById('main-links');
+
+    // create table row
+    const trow = document.createElement('tr');
+    trow.id = id + '-thread';
+
+    // create table header
+    const th = document.createElement('th');
+    th.scope = 'col';
+    const preview = document.createElement('a');
+    preview.id = id + '-link';
+    preview.href = url;
+    preview.innerHTML = alias;
+    preview.target = "_blank";
+    th.appendChild(preview);
+
+    // create table data
+    const tdAlias = document.createElement('td');
+    const tdAliasInput = document.createElement('input');
+    tdAliasInput.type = 'text';
+    tdAliasInput.id = id + '-alias';
+    tdAliasInput.value = alias;
+    tdAliasInput.classList.add("form-control");
+    tdAliasInput.oninput = () => { updatePreview(id) };
+    tdAlias.appendChild(tdAliasInput);
+
+    const tdUrl = document.createElement('td');
+    const tdUrlInput = document.createElement('input');
+    tdUrlInput.type = 'text';
+    tdUrlInput.id = id + '-url';
+    tdUrlInput.value = url;
+    tdUrlInput.classList.add("form-control");
+    tdUrlInput.oninput = () => { updatePreview(id) };
+    tdUrl.appendChild(tdUrlInput);
+
+    // create update / remove buttons
+    const tdUpdate = document.createElement('td');
+    tdUpdate.id = id + '-update';
+    const tdUpdateButton = document.createElement('button');
+    tdUpdateButton.onclick = () => { updateLink(id) };
+    tdUpdateButton.innerHTML = "Update";
+    tdUpdateButton.classList.add("btn");
+    tdUpdateButton.classList.add("btn-primary");
+    tdUpdate.appendChild(tdUpdateButton);
+
+    const tdRemove = document.createElement('td');
+    tdUpdate.id = id + '-remove';
+    const tdRemoveButton = document.createElement('button');
+    tdRemoveButton.onclick = () => { removeLink(id) };
+    tdRemoveButton.innerHTML = "Remove";
+    tdRemoveButton.classList.add("btn");
+    tdRemoveButton.classList.add("btn-danger");
+    tdRemove.appendChild(tdRemoveButton);
+
+    trow.appendChild(th);
+    trow.appendChild(tdAlias);
+    trow.appendChild(tdUrl);
+    trow.appendChild(tdUpdate);
+    trow.appendChild(tdRemove);
+
+    mainLinks.appendChild(trow);
+
 }
