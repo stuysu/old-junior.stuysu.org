36d04ff268016482ff26ce1fb43a460651afe370
Myles Pasetsky <mp3blue99@gmail.com>
2021-02-02 13:16:36 -0500

    Remove double tracking from admin route

--- routes/docs/admin.js
+++ routes/docs/admin.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { analytics } = require("../utils");
 const route = express.Router();
 
 const IS_ADMIN_ROUTE =
@@ -9,8 +8,6 @@ const ADMIN_ROUTE = process.env.ADMIN_ROUTE;
 
 route.get(
     IS_ADMIN_ROUTE ? `/admin/${ADMIN_ROUTE}` : `/admin`,
-
-    analytics,
 
     (req, res, next) => {
         res.render("admin/", { client_id: process.env.CLIENT_ID });
