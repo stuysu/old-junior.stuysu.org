43a9204c0c6e003f6da5895472fb4525c904ce49
Myles Pasetsky <mp3blue99@gmail.com>
2021-08-23 15:09:58 -0400

    Change to new google authen, and jwt authorization

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -33,9 +33,14 @@ async function forceLoad() {
 // Renders the admin response with no authentication 
 async function withoutAuthentication(req, res, next) {
     try {
-        res.render('admin/response', await forceLoad());
+        res.render('admin/response', await forceLoad(), (err, html) => {
+            if (err)
+                throw err;
+
+            res.render('admin/no-load', {response: html});
+        });
     } catch(err) {
-        res.end('<h3>Error: Could not load data</h3>');
+        res.type('html').end('<h3>Error: Could not load data</h3>');
     }
 }
 
@@ -50,54 +55,77 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(CLIENT_ID);
 
-const verifyToken = async (id_token) => {
-    const ticket = await client.verifyIdToken({
-        idToken: id_token,
-        audience: CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
-
-    if (
-        payload["aud"] === CLIENT_ID &&
-        (payload["iss"] === "accounts.google.com" ||
-            payload["iss"] === "https://accounts.google.com")
-    ) {
-        return payload;
-    } else {
-        throw new Error("Cannot verify audience and/or issuer");
-    }
-};
-
-// This code wraps the google authentication around 
-// no-auth verification 
-async function withAuthentication(req, res, next) {
-    const id_token = req.body.id_token;
-
+async function verifyToken(token) {
     try {
-        let payload = await verifyToken(id_token);
-        
-        let validated = true;
-        
-        if (AUTH_MODE.shouldAuth()) {
-            const { sub } = payload;
-            const tmp = await Subs.findOne({ where: { sub: sub } });
-            validated = (tmp !== null);
-        }
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload();
 
         if (process.env.NODE_ENV !== 'production') {
             console.log(`${payload.given_name} ${payload.family_name} (${payload.email}) has sub token ${payload.sub}`);
-            if (AUTH_MODE.shouldAuth()) console.log(`They are ${validated?'':'not'} in the database.`);
         }
 
-        if (validated) {
+        if (AUTH_MODE.shouldAuth()) {
+            const { sub } = ticket.getPayload();
+            const usr = await Subs.findOne({ where: { sub: sub } });
+
+            let ok = (usr !== null);
+
+            if (AUTH_MODE.shouldAuth()) 
+                console.log(`They are ${ok?'':'not'} in the database.`);
+
+            return { ok: ok, token: token };
+        } else {
+            return { ok: true, token: token };
+        }
+    } catch (err) {
+        console.error(err);
+        return { ok: false, token: token };
+    }
+}
+
+
+async function checkDoubleCookies(req) {
+    // Check if double cookies are working
+    let g_csrf_token = req.cookies['g_csrf_token'];
+    if (g_csrf_token === undefined)
+        throw new Error('No CSRF token in Cookie');
+    
+    let g_csrf_body = req.body['g_csrf_token'];
+    if (g_csrf_body === undefined)
+        throw new Error('No CSRF token in body');
+    
+    if (g_csrf_body != g_csrf_token)
+        throw new Error('Failed to verify double CSRF tokens');
+
+    return true;
+}
+
+// This code wraps the google authentication around 
+// no-auth verification 
+
+async function withAuthentication(req, res, next) {
+    try {
+        await checkDoubleCookies(req);
+        let { ok, token } = await verifyToken(req.body.credential);
+
+        if (ok) {
+            res.cookie('jid', token, {
+                httpOnly: true, 
+                sameSite: true, 
+                // path: '/admin'
+            });
             await withoutAuthentication(req, res, next);
         } else {
-            res.end("<h3>Error: Could not authenticate</h3>");
+            res.type('html').end("<h3>Error: Could not authenticate</h3>");
         }
-    
     } catch (err) {
-        err.error = err.error || true;
-        res.status(400).json(err);
+        console.error(err);
+        // res.status(400).json(err);
+        res.type('html').end("<h3>GET THE FUCK OUT OF HERE</h3>");
     }
 }
 
--- routes/docs/admin.js
+++ routes/docs/admin.js
@@ -3,26 +3,33 @@ const route = express.Router();
 
 const { AUTH_MODE, analyticsOn } = require("../utils.js");
 
-const HAS_ADMIN_ROUTE =
-    process.env.ADMIN_ROUTE !== undefined && process.env.ADMIN_ROUTE.length > 0;
-
-const ADMIN_ROUTE = process.env.ADMIN_ROUTE;
+route.get('/admin/signin', (req, res, next) => {
+    res.render("admin/", {client_id: process.env.CLIENT_ID});
+});
 
 route.get(
-    HAS_ADMIN_ROUTE ? `/admin/${ADMIN_ROUTE}` : `/admin`,
+    '/admin',
 
-    analyticsOn(
-        "Admin Homepage",
+    async (req, res, next) => {
+        console.log('HELLO');
 
-        (req, res, next) => {
-
-            if (AUTH_MODE.shouldAsk())
-                res.render("admin/", { client_id: process.env.CLIENT_ID });
-            else // if (AUTH_MODE.shouldSkip())
-                res.render("admin/no-auth");
-
+        if (AUTH_MODE.shouldSkip()) {
+            next();
+        } else {
+            res.redirect('/admin/signin');
         }
-    )
+    },
+
+    async (req, res, next) => {
+        // if (req.cookies.get(''))
+        res.json(req.cookies['jid']);
+
+        // if (AUTH_MODE.shouldAsk())
+        //     res.render("admin/", { client_id: process.env.CLIENT_ID });
+        // else // if (AUTH_MODE.shouldSkip())
+        //     res.type('html').send('<h3>You good bruh</h3>');
+            // await withoutAuthentication(req, res, next);
+    }
 );
 
 module.exports = route;
