24b58c7cc37a0691165cf30edf921bd767b2739a
Myles Pasetsky <mp3blue99@gmail.com>
2021-08-30 20:50:59 -0400

    Add cookie utiity class

---
+++ routes/cookie.js
@@ -0,0 +1,32 @@
+// Encapsulates common functionality of a cookie
+
+class Cookie {
+    constructor(name, options) {
+        this.name = name;
+        this.options = options;
+    }
+
+    isSigned() {
+        return this.options.signed;
+    }
+
+    get(req) {
+        if (this.isSigned()) {
+            return req.signedCookies[this.name];
+        }
+        return req.cookies[this.name];
+    }
+
+    set(res, value) {
+        res.cookie(this.name, value, this.options);
+    }
+
+    clear(res) {
+        res.clearCookie(this.name, { 
+            path: this.options.path
+            // domain: ??? 
+        });
+    }
+};
+
+module.exports = { Cookie };
